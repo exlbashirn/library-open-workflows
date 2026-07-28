c6cd8af0b9b7bb131480aa7917d714fe4ea3b859
exlbashirn <55796460+exlbashirn@users.noreply.github.com>
2026-07-28 14:48:45 +0300

    Fix iframe height calculation and add empty list component styles

--- cloudapp/src/app/shared/empty-list/empty-list.component.ts
+++ cloudapp/src/app/shared/empty-list/empty-list.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-empty-list',
-  templateUrl: './empty-list.component.html'
+  templateUrl: './empty-list.component.html',
+  styleUrls: ['./empty-list.component.scss']
 })
 export class EmptyListComponent {
 
